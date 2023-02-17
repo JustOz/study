38c081902b1c755fd2f7e632de951af68b831782
oded zrubavel <odedzrubavel@gmail.com>
2023-02-17 23:14:26 +0200

    kool

--- DevOps/k8s/get.sh
+++ DevOps/k8s/get.sh
@@ -3,22 +3,12 @@
 
 ./installation-scripts/docker/get-docker.sh
 
-sleep 1
-
 ./installation-scripts/k8s/get-k8s.sh
-
-sleep 1
 
 ./installation-scripts/k8s/get-and-save-images.sh
 
-sleep 1
-
 ./installation-scripts/k8s/get-and-save-flannel.sh
 
-sleep 1
-
 ./installation-scripts/k8s/get-and-save-nginx.sh
-
-sleep 1
 
 ./installation-scripts/helm/get-helm.sh
